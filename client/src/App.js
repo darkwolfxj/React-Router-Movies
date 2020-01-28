3b98de6fc2cbc99bf91783a055d0ca9a758c4d87
darkwolfxj <hesitation.dotdot@gmail.com>
2020-01-28 14:48:53 -0500

    Stretched

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,12 @@ import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
 
+
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+  setSavedList( [...savedList, movie] );
   };
 
   return (
@@ -18,7 +19,7 @@ const App = () => {
           <Route exact path='/'>
               <MovieList />
           </Route>
-          <Route path='/movies/:id' component={Movie} />
+          <Route path='/movies/:id' render={props => ( <Movie {...props} addToSavedList={addToSavedList}/>)}/>
           
       </div>
     </div>
